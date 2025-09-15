53dca7ff311221bceb40dd4be1e346a1da77dd88
jennasaikaly <jen.saikaly@gmail.com>
2025-09-15 12:12:26 -0400

    created POST route, tested POST AND GET route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -15,4 +15,18 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+  // expects {username: 'Saikaly', email: 'jenna@gmail.com', password: 'password1234'}
+    User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
