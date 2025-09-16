42810590489a00bd53f51a8ae51851023716819d
jennasaikaly <jen.saikaly@gmail.com>
2025-09-16 19:06:57 -0400

    created Comment Model and get/post/delete routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 //collects the user endpoints and prefixes them with the path /users
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
