5e179770059e966a3ddd390d5f32e69e76db5c9a
jennasaikaly <jen.saikaly@gmail.com>
2025-09-23 15:26:38 -0400

    added logic to destroy the session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -102,10 +102,20 @@ router.post('/login', (req, res) => {
             req.session.loggedIn = true;
 
             res.json({ user: dbUserData, message: 'You are now logged in!' });
-        });
-
+            });
         });
     })
+
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });    
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
 //PUT /api/users/1
 router.put('/:id', (req, res) => {
