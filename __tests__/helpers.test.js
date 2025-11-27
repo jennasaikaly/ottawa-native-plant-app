43be2cfc6555b463ababd9e4a16ca0aa786ebcdf
jennasaikaly <jen.saikaly@gmail.com>
2025-11-27 15:08:25 -0500

    fixed tests

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,6 +1,13 @@
-const format_date = require('../utils/helpers');
-test('format_date() returns a date string', () => {
-  const date = new Date('2020-03-20 16:12:03');
+const { format_date, format_plural } = require('../utils/helpers');
 
-  expect(format_date(date)).toBe('3/20/2020');
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+
+    expect(format_date(date)).toBe('3/20/2020');
 });
+
+test('format_plural correctly adds an s to any number other than 1', () => {
+    const word = "tiger";
+    const amount = 2;
+    expect(format_plural(word, amount)).toBe('tigers');
+})
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,26 +1,34 @@
-// module.exports = {
-//   format_date: date => {
-//     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-//       date
-//     ).getFullYear()}`;
-//   }
-// }
 
-
-// function sum(a, b) {
-//   return a + b;
-// }
-// module.exports = sum;
-
-let date = new Date('2020-03-20 16:12:03')
-
-function format_date(){
+// this works (2 functions)
+function format_date(date){
 
 return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-      date
+    date
     ).getFullYear()}`;
 }
 
-module.exports = format_date;
+function format_plural(word, amount){
+    if (amount !== 1){
+        return `${word}s`;
+    }else {
+        return word;
+    }
+}
+module.exports = { format_date, format_plural }
 
-
+// this also works (2 functions)
+// module.exports = {
+//     format_date: date => {
+//         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+//         date
+//         ).getFullYear()}`;
+//     },
+//     format_plural: (word, amount) => {      
+//         if (amount !== 1){
+//         return `${word}s`;
+//     }else {
+//         return word;
+//     }
+    
+// }
+// }   
