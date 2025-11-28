505a63489574a5bfb1e8f3ce98f006b83f4a35de
jennasaikaly <jen.saikaly@gmail.com>
2025-11-28 14:21:52 -0500

    added ability to edit title of post

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,47 +1,9 @@
-// async function newFormHandler(event) {
-//     // debugger;
-//     event.preventDefault();
-//     const title = document.querySelector('input[name="post-title"]')
-//     // const post_text = document.querySelector('textarea[name="new-post-body"]').value.trim();
-//     const post_url = document.querySelector('input[name="post-url"]')
-//     // const user_id  = req.params.id
-//     // const post_id = window.location.toString().split('/')[
-//     //     window.location.toString().split('/').length - 1
-//     //     ];
-    
-    
-//         const response = await fetch('/api/posts', {
-        
-//             method: 'POST',
-//             body: JSON.stringify({
-//                 // post_id,
-//                 // post_text,
-//                 title,
-//                 post_url,
-//                 // user_id
-//             }),
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         });
-
-//         if (response.ok) {
-//             document.location.reload();
-//         } else {
-//             alert(response.statusText);
-//         }
-//     }
-
-
-// document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
-
 async function newFormHandler(event) {
   event.preventDefault();
 debugger;
   const title = document.querySelector('input[name="new-post-title"]').value;
   const post_url = document.querySelector('input[name="new-post-url"]').value;
   const post_text = document.querySelector('textarea[name="new-post-body"]').value.trim();
-//   const post_text = document.querySelector('input[name="text"]')
   const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
---
+++ public/javascript/edit-post.js
@@ -0,0 +1,45 @@
+async function editPostClickHandler(event) {
+  // debugger;
+  event.preventDefault();
+  const title = document.querySelector('input[name="edit-post-title"]').value;
+  // const post_url = document.querySelector('input[name="edit-post-url"]').value;
+  // const post_text = document.querySelector('textarea[name="edit-post-body"]').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch(`/api/posts/${id}`, {
+  method: 'PUT',
+  body: JSON.stringify({
+    title,
+    // post_url,
+    // post_text,
+    id,    
+  }),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+if (response.ok) {
+  document.location.replace('/dashboard')
+  // document.location.reload();
+  // document.location.replace(`/post/${id}`);
+} else {
+  alert(response.statusText);
+}
+}
+
+// async function deletePostClickHandler(event){
+// event.preventDefault(); 
+
+// const id = window.location.toString().split('/')[
+//     window.location.toString().split('/').length - 1
+//   ];
+
+// const response = await fetch('api/posts/')
+
+
+// }
+
+document.querySelector('.edit-post-btn').addEventListener('click',editPostClickHandler);
+// document.querySelector('.delete-post-btn').addEventListener('click',deletePostClickHandler);
