46dbea6d4b56da059be20ce89011429cb4f72241
jennasaikaly <jen.saikaly@gmail.com>
2026-01-08 17:03:49 -0500

    added Post Controller

---
+++ vite/ottawa-native-plant-app/server/models/Post.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const PostSchema = new Schema({
+    post_title: {
+        type: String
+    },
+    post_url: {
+        type: String
+    }, 
+    post_text: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    }, 
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Post = model('Post', PostSchema);
+module.exports = Post;
--- vite/ottawa-native-plant-app/server/server.js
+++ vite/ottawa-native-plant-app/server/server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const dbo = require("./db/connection.js")
-
+const mongoose = require('mongoose')
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes"))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ottawa-native-plant-app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
 const PORT = 3000;
 // const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
