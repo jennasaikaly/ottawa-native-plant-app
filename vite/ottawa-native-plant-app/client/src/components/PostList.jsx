6e784aa3499f14b369b5e1869f89471c20b81ab6
jennasaikaly <jen.saikaly@gmail.com>
2026-01-10 12:33:43 -0500

    connected post component to db

--- vite/ottawa-native-plant-app/client/src/components/PostList.jsx
+++ vite/ottawa-native-plant-app/client/src/components/PostList.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PostCard from './PostCard/PostCard'
 
-const posts = [
-    {   
-            "id": 1,
-            "username": "Midnight",
-            "post_title": "I need coffee!",
-            "post_text": "Where is my cup?",
-	        "post_url": "Checkthecupboard.com"	        
-        }, 
-        {   
-            "id": 2,
-            "username": "Midnight",
-            "post_title": "I need food",
-            "post_text": "Where is my plate?",
-	        "post_url": "Checkthedishwasher.com"	        
-        },
-        {   
-            "id": 3,
-            "username": "Midnight",
-            "post_title": "I need a bite!",
-            "post_text": "Where is my spoon?",
-	        "post_url": "Checkthedrawer.com"	        
-        }
-    ]
+// const posts = [
+//     {   
+//             "id": 1,
+//             "username": "Midnight",
+//             "post_title": "I need coffee!",
+//             "post_text": "Where is my cup?",
+// 	        "post_url": "Checkthecupboard.com"	        
+//         }, 
+//         {   
+//             "id": 2,
+//             "username": "Midnight",
+//             "post_title": "I need food",
+//             "post_text": "Where is my plate?",
+// 	        "post_url": "Checkthedishwasher.com"	        
+//         },
+//         {   
+//             "id": 3,
+//             "username": "Midnight",
+//             "post_title": "I need a bite!",
+//             "post_text": "Where is my spoon?",
+// 	        "post_url": "Checkthedrawer.com"	        
+//         }
+//     ]
 
     export default function PostList(){
+
+       
+            const [ posts, setPosts ] = useState([]);
+
+            useEffect(() => {
+                fetch('http://localhost:3000/api/posts')
+                .then(response => response.json())
+                .then(data => setPosts(data))
+                .catch(error => console.error(`Error:`, error));
+            }, []);
+           
         return (
             <div className="post-list-container">
                 <ul>
