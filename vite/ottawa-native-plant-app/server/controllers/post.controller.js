72032c492277d9b8ae7df05d6b79b1883d93560c
jennasaikaly <jen.saikaly@gmail.com>
2026-01-13 13:34:56 -0500

    cleaned up Post routes and controller

--- vite/ottawa-native-plant-app/server/controllers/post.controller.js
+++ vite/ottawa-native-plant-app/server/controllers/post.controller.js
@@ -1,5 +1,6 @@
 import { Post } from '../models/index.js'
 
+// GET ALL POSTS
 export const getAllPosts = (req, res) => {
     Post.find({})
         .then (console.log('i made it'))
@@ -10,14 +11,15 @@ export const getAllPosts = (req, res) => {
         })        
 }
 
-    //CREATE POST 
+// CREATE POST 
 export const createPost = (req, res) => {
     Post.create(req.body)
         .then(dbPostData => res.json(dbPostData))
         .catch(err => res.status(400).json(err));
 }
 
-export const getOnePostById = (req, res) => {
+//GET POST BY ID
+export const getPostById = (req, res) => {
     console.log({})
     Post.findById(req.params.id)
         .then(console.log('i am here'))
@@ -31,7 +33,7 @@ export const getOnePostById = (req, res) => {
         .catch(err => res.status(400).json(err))
 }
 
-     //UPDATE POST BY ID
+// GET POST BY ID AND UPDATE
 export const updatePost = (req, res) => {
     Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
         .then(console.log('i am updating'))
@@ -40,6 +42,7 @@ export const updatePost = (req, res) => {
         .catch(err => res.status(400).json(err))
 }
 
+// GET POST BY ID AND DELETE
 export const deletePost = (req, res) => {
     Post.findByIdAndDelete(req.params.id)
         .then(console.log('i am deleting'))       
@@ -54,20 +57,6 @@ export const deletePost = (req, res) => {
         .catch(err => res.status(400).json(err))
 
 }
-// // delete Post
-// deletePost({ params }, res) {
-//   Post.findOneAndDelete({ _id: params.id })
-//     .then(dbPostData => {
-//       if (!dbPostData) {
-//         res.status(404).json({ message: 'No Post found with this id!' });
-//         return;
-//       }
-//       res.json(dbPostData);
-//     })
-//     .catch(err => res.status(400).json(err));
-// }
-
-
 // //     //GET ONE POST BY ID using await/async
 // export const getOnePostById = async (req, res, next) => {
 //     try{ 
--- vite/ottawa-native-plant-app/server/routes/api/post-routes.js
+++ vite/ottawa-native-plant-app/server/routes/api/post-routes.js
@@ -1,11 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { getAllPosts } from '../../controllers/post.controller.js'
-import { createPost } from '../../controllers/post.controller.js'
-import { getOnePostById } from '../../controllers/post.controller.js'
-import { updatePost, deletePost } from '../../controllers/post.controller.js'
-// import { getOnePostById } from '../../controllers/post.controller.js'
-// import { Post } from '../../models/index.js'
+import { getAllPosts, createPost, getPostById, updatePost, deletePost } from '../../controllers/post.controller.js'
 // import withAuth from '../../utils/auth.js'
 
 // // Set up GET all and POST at /api/posts
@@ -17,8 +12,8 @@ router
 // // Set up GET one, PUT, and DELETE at /api/posts/:id
 router
   .route('/:id')
-  .get(getOnePostById)
+  .get(getPostById)
   .put(updatePost)
   .delete(deletePost);
 
- export default router;
+export default router;
